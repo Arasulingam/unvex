9fbae85b203a54ded0443e494d4252f2e4b31fac
Arasu Lingam <87162248+Arasulingam@users.noreply.github.com>
2024-06-17 20:19:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <img src="public\icon.png" className="logo" alt="Vite logo" />
+      
       <div>Under Development</div>
     </>
   );
